6a7390bdf063a0da17b7c993f039cf809e694f94
kawatech <kawatech@gol.com>
2020-01-01 08:28:03 +0900

    Initial commit2020

---
+++ app/src/main/java/jp/techacademy/naoki/kawamata/kotlinlog/Dog.kt
@@ -0,0 +1,18 @@
+package jp.techacademy.naoki.kawamata.kotlinlog
+import android.util.Log
+open class Dog {
+    // プロパティ
+    var name: String
+    var age: Int
+
+    // 引数付きコンストラクタ
+    constructor(name: String, age: Int) {
+        this.name = name
+        this.age = age
+    }
+
+    // メソッド
+    open fun say() {
+        Log.d("kotlintest", this.name + "(" + this.age + "歳)" + "「ワンワン」")
+    }
+}
